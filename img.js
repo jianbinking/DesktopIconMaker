d08eb0e949d64fecfd597f1a1bd35a2a485a791e
WangJianBin <403317854@qq.com>
2021-03-04 16:51:40 +0800

    .

--- img.js
+++ img.js
@@ -1,33 +1,66 @@
 var fs = require("fs");
 var gm = require("gm");
 
-var imgPath = './IMG_2481.PNG'
+
+let cfg = {
+  iphone7: {
+    screen_width: 374,
+    screen_height: 888,
+    icon_width: 128,
+    icon_height: 128,
+    edge_h: 156,
+    edge_v: 64,
+    space_h: 62,
+    space_v: 96
+  },
+  iphone11: {
+    screen_width: 828,
+    screen_height: 1792,
+    icon_width: 128,
+    icon_height: 128,
+    edge_h: 64,
+    edge_v: 160,
+    space_h: 62,
+    space_v: 82
+  },
+}
+
+var imgPath = './source.jpg'
 var savedFolder = './icons'
 
 var rowCount = 6;
 var columnCount = 4;
+let profile = cfg.iphone11
 
-var iconw = 128;
-var iconh = 128;
-var edgetop = 156;
-var edgeleft = 64;
-var spaceH = 62;
-var spaceV = 96;
+let imgSource = gm(imgPath)
+imgSource.size((err, value) => {
+  let scale = Math.max(value.width / profile.screen_width, value.height / profile.screen_height)
 
-for (i = 0; i < rowCount; i++) {
-  for (j = 0; j < columnCount; j++) {
-    xoff = edgeleft + j * (iconw + spaceH);
-    yoff = edgetop + i * (iconh + spaceV);
-    box = (xoff, yoff, xoff + iconw, yoff + iconh);
-    
-    var savePath = savedFolder + "/" + (i + 1) + (j + 1) + ".png";
-    console.log("准备保存:", savePath)
-    gm(imgPath).crop(iconw, iconh, xoff, yoff).write(savePath, (err, buffer) => {
-      if (err) {
-        console.log("保存失败", err)
-      } else {
-        console.log("保存成功")
-      }
-    })
+  let icon_width = profile.icon_width * scale
+  let icon_height = profile.icon_height * scale
+  let edge_h = profile.edge_h * scale
+  let edge_v = profile.edge_v * scale
+  let space_h = profile.space_h * scale
+  let space_v = profile.space_v * scale
+
+  for (i = 0; i < rowCount; i++) {
+    for (j = 0; j < columnCount; j++) {
+      let xoff = edge_h + j * (icon_width + space_h);
+      let yoff = edge_v + i * (icon_height + space_v);
+      let box = (xoff, yoff, xoff + icon_width, yoff + icon_height);
+      
+      var savePath = savedFolder + "/" + (i + 1) + (j + 1) + ".png";
+      console.log("准备保存:", savePath)
+      gm(imgPath).crop(icon_width, icon_height, xoff, yoff).write(savePath, (err, buffer) => {
+        if (err) {
+          console.log("保存失败", err)
+        } else {
+          console.log("保存成功")
+        }
+      })
+    }
   }
-}
+
+})
+
+
